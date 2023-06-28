a550e95782189a7e85ca2b15eedd896568e980e7
Eren Yilmaz <erenyilmaz95@gmx.de>
2023-06-28 10:02:59 +0200

    Refactor ImageDataProvider

--- data/image_metadata.py
+++ data/image_metadata.py
@@ -1,32 +1,41 @@
 import datetime
 import json
 import os
+from abc import ABCMeta, abstractmethod
 from typing import Literal
 
 from lib.util import EBC
 
 
-class ImageMetadata(EBC):
-    def __init__(self, created_by_provider: str, dt_created: float, image_type: Literal['png', 'jpg']):
-        self.dt_created = dt_created
-        self.created_by_provider = created_by_provider
+class ImageMetadata(EBC, metaclass=ABCMeta):
+    def __init__(self, image_type, created_by_provider):
         self.image_type = image_type
+        self.created_by_provider = created_by_provider
 
     def image_filename(self):
-        return self._base_name() + '.' + self.image_type
+        return self.base_file_name() + '.' + self.image_type
 
     def json_filename(self):
-        return self._base_name() + '.json'
+        return self.base_file_name() + '.json'
 
     def json_path(self):
         return os.path.join('img', self.json_filename())
 
-    def _base_name(self):
-        return f'{self.created_by_provider}_{self.dt_str()}'
-
-    def dt_str(self) -> str:
-        return datetime.datetime.fromtimestamp(self.dt_created).strftime('%Y-%m-%d_%H-%M-%S')
+    def base_file_name(self):
+        raise NotImplementedError('Abstract method')
 
     def save_to_disk(self):
         with open(self.json_path(), 'w') as f:
             json.dump(self.to_json(), f, indent=4)
+
+
+class ImageMetadataFromVideo(ImageMetadata):
+    def __init__(self, created_by_provider: str, dt_created: float, image_type: Literal['png', 'jpg']):
+        super().__init__(created_by_provider=created_by_provider, image_type=image_type)
+        self.dt_created = dt_created
+
+    def dt_str(self) -> str:
+        return datetime.datetime.fromtimestamp(self.dt_created).strftime('%Y-%m-%d_%H-%M-%S')
+
+    def base_file_name(self):
+        return f'{self.created_by_provider}_{self.dt_str()}'
--- video_to_img/image_data_provider.py
+++ video_to_img/image_data_provider.py
@@ -1,7 +1,7 @@
 import os.path
 from typing import List
 
-from data.image_metadata import ImageMetadata
+from data.image_metadata import ImageMetadataFromVideo
 
 
 class ImageDataProvider:
@@ -17,5 +17,5 @@ class ImageDataProvider:
     def json_paths(self) -> List[str]:
         return [os.path.splitext(p)[0] + '.json' for p in self.image_paths()]
 
-    def image_metadata_list(self) -> List[ImageMetadata]:
-        return [ImageMetadata.from_json_file(p) for p in self.json_paths()]
+    def image_metadata_list(self) -> List[ImageMetadataFromVideo]:
+        return [ImageMetadataFromVideo.from_json_file(p) for p in self.json_paths()]
