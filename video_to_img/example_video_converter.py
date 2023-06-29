6ee0369989319f663d6ee348c8bf9e92930e958b
Eren Yilmaz <erenyilmaz95@gmx.de>
2023-06-29 11:31:28 +0200

    Do not overwrite by default

--- video_to_img/example_video_converter.py
+++ video_to_img/example_video_converter.py
@@ -5,6 +5,6 @@ from video_to_img.video_to_image_converter import VideoToImageConverter
 
 
 class ExampleVideoToImageConverter(VideoToImageConverter):
-    def __init__(self, overwrite=True):
+    def __init__(self, overwrite=False):
         super().__init__(video_path=os.path.join(resources.resource_dir_path(), 'VIDEO_CAPTIONING_DEMO_VIDEO.mp4'),
                          overwrite=overwrite)
